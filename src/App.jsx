34086365cdfb6e05209b8e7d25ed8ce3bf04b8d2
Kirill Bubentsov <kirill.dm6@gmail.com>
2017-05-12 04:16:37 +0300

    start build

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.scss';
+
+const App = React.createClass({
+
+    render:function(){
+        return(
+          <h1>App</h1>
+        )
+    }
+});
+
+export default App;
+
